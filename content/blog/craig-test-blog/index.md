d4480d6889049d2b4d3506d36668f05f0c530987
cneeson <craigneeson@hotmail.co.uk>
2019-01-18 16:07:00 +0000

    Initial Commit

---
+++ content/blog/craig-test-blog/index.md
@@ -0,0 +1,7 @@
+---
+title: Craig Test Blog
+date: '2018-05-06T23:46:37.121Z'
+timeToRead: '2 minutes'
+---
+
+Test words and things
---
+++ src/components/Link.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Link as GatsbyLink } from 'gatsby'
+
+const Link = styled(GatsbyLink)`
+    color: #ff007d;
+`;
+
+export default Link;
