6302e6a59d01cfff550275039a364b3b56dbaa2c
cneeson <craigneeson@hotmail.co.uk>
2019-11-27 15:23:23 +0000

    Added design constraints blog

---
+++ content/blog/externalBlogs.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        node: {
+            slug: 35,
+            isPublished: false,
+            frontmatter: {
+                title: 'Balancing design constraints and developer experience in React Component Libraries',
+                isNote: false,
+                date: '2019-09-05T23:59:59.121Z',
+                timeToRead: '7 minute',
+
+            },
+            fields: {
+                isExternalBlog: true,
+                externalUrl: 'https://community.flexera.com/t5/Flexera-Engineering-Blog/Balancing-design-constraints-and-developer-experience-in-React/ba-p/114181',
+            },
+            excerpt: `One of the greatest challenges for Flexera's UI developers has been writing frontends that look and behave consistently across the entire product portfolio. In 2019, Flexera's UI and UX teams have worked in collaboration to release a comprehensive suite of React components that can be used to unify the company's products.`
+        }
+    }
+];
