785a409e3344d9409a3a064b7044cac206f619e0
cneeson <craigneeson@hotmail.co.uk>
2019-12-15 20:22:26 +0000

    Updating bio, a few styling updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,12 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          `gatsby-remark-prismjs`,
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              showLineNumbers: true,
+            }
+          },
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
         ],
@@ -59,11 +64,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Craig Neeson Personal Blog`,
-        short_name: `CN Blog`,
+        name: `Craig Neeson`,
+        short_name: `CN`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#ff007d`,
         display: `minimal-ui`,
         icon: `content/assets/favicon.png`,
       },
--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -23,19 +23,19 @@ function Bio() {
                             <Tagline>
                                 Hi, I'm <strong>{data.site.siteMetadata.author}</strong>.
                             <br />
-                                I am a software engineer based in Belfast, Northern Ireland.
+                                I am a UI Engineer based in Belfast, Northern Ireland.
                             </Tagline>
                         </div>
                     </IntroSection>
                     <BioSection
                         title="Code"
                         visualSource={codeSvgUrl}
-                        content="My primary passion is working in the UI. I have extensive experience building rich, data-centric user experiences for the web using a variety of stacks. The technologies that I am most familiar with nowadays are Javascript and React alongside CSS in JS packages such as styled-components. Although my focus at this point in my career is to do great front-end work, I like to keep my field knowledge diverse. My interests lie in every aspect of the way people build things using technology. You can find my full details of my work history in my CV."
+                        content="I am a software engineer with a passion for UI. I am interested in building beautiful, data-centric user experiences for web and mobile. The technologies that I am most familar with are JavaScript, TypeScript and progressive CSS-in-JS frameworks such as styled-components and emotion. I have a deep interest in building highly scalable front-ends, employing the latest tools and processes to build the best possible product. I enjoy delivering UIs just as much as I enjoy thinking through foundational concepts for front-end teams to thrive. These concepts include an interest in design systems, continuous delivery, staying on top of the latest tech and writing robust documentation."
                     />
                     <BioSection
                         title="Projects"
                         visualSource={projectsSvgUrl}
-                        content="I have been fortunate enough to be involved in the creation and maintenance of web-applications for many industries across Northern Ireland including manufacture, healthcare and e-commerce. Most of these projects have been fully bespoke, tailored to the specific business-needs of the customer in question, big or small. I also have worked to develop highly scalable microservices for the US insurance consumer market, capable of processing millions of requests in an acceptable timeframe. My latest role is as a UI engineer, building dashboards to query and visualise one of the largest datasets in the world. "
+                        content="I have been fortunate enough to be involved in the creation and maintenance of web-applications for many industries across Northern Ireland including manufacture, healthcare and e-commerce. Most of these projects have been fully bespoke, tailored to the specific business-needs of the customer in question. I also have worked to develop highly scalable microservices for the US insurance consumer market, capable of processing millions of requests in an acceptable timeframe. Other products include building dashboards to query and visualise one of the largest datasets in the world, and most recently to create a next generation recruitment platform for healthcare professionals across the UK."
                     />
                     <ContactSection />
                 </Fragment>
